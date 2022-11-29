aac9573ca0c2a0eaf3a02e574dda52eb25cfdce1
Jose Dias <josediasbn1@gmail.com>
2022-11-29 17:46:36 -0300

    teste req 6

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,6 +6,8 @@ import renderWithRouter from '../helpers/renderWithRouter';
 
 test('1 - Test de Login', () => {
   renderWithRouter(<App />);
+  jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem');
+  Object.setPrototypeOf(window.localStorage.setItem, jest.fn());
   const emailInput = screen.getByTestId('email-input');
   const passInput = screen.getByTestId('password-input');
   const buttonInput = screen.getByTestId('login-submit-btn');
@@ -16,4 +18,6 @@ test('1 - Test de Login', () => {
   userEvent.type(emailInput, 'jose@gmail.com');
   userEvent.type(passInput, '1234567');
   expect(buttonInput).toBeEnabled();
+  userEvent.click(buttonInput);
+  expect(window.localStorage.setItem).toHaveBeenCalled();
 });
