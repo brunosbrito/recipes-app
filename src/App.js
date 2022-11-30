6837e3bcd22e177dd440f148fadbeccecd57ab27
Jose Dias <josediasbn1@gmail.com>
2022-11-30 15:15:42 -0300

    reqs 57-62

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/profile" render={ () => <Profile /> } />
     </Switch>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Profile() {
+  const history = useHistory();
+  const profileEmail = localStorage.getItem('user');
+  const profileEmailParse = JSON.parse(profileEmail);
+  const logoutFunc = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+  return (
+    <>
+      <h2 data-testid="profile-email">{ profileEmailParse.email }</h2>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ logoutFunc }
+      >
+        Logout
+      </button>
+    </>
+  );
+}
+
+export default Profile;
