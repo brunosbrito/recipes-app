12f180300b5b9663e5ab286092a4b1eb763a0cb1
Bruno <brunobrito728@gmail.com>
2022-12-06 19:55:07 -0300

    [WIP]requisito 30 em andamento (adcionado as receitas no localStorage)

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-// import RecipesContext from '../context/RecipesContext';
 import '../CSS/RecipeInProgress.css';
 
 function RecipeInProgress() {
@@ -9,67 +8,10 @@ function RecipeInProgress() {
 
   const [disabled, setDisabled] = useState(true);
   const [counter, setCounter] = useState(0);
-  // const { recipesInProgress } = useContext(RecipesContext);
   const dataRecipe = JSON.parse(localStorage.getItem('recipe'));
   const { id } = useParams();
-
-  // const recipe = [
-  //   {
-  //     idMeal: '52977',
-  //     strMeal: 'Corba',
-  //     strDrinkAlternate: null,
-  //     strCategory: 'Side',
-  //     strArea: 'Turkish',
-  //     strInstructions: 'Pick through your lentils for any foreign debris, rinse them 2 or 3 times, drain, and set aside.  Fair warning, this will probably turn your lentils into a solid block that you’ll have to break up later\r\nIn a large pot over medium-high heat, sauté the olive oil and the onion with a pinch of salt for about 3 minutes, then add the carrots and cook for another 3 minutes.\r\nAdd the tomato paste and stir it around for around 1 minute. Now add the cumin, paprika, mint, thyme, black pepper, and red pepper as quickly as you can and stir for 10 seconds to bloom the spices. Congratulate yourself on how amazing your house now smells.\r\nImmediately add the lentils, water, broth, and salt. Bring the soup to a (gentle) boil.\r\nAfter it has come to a boil, reduce heat to medium-low, cover the pot halfway, and cook for 15-20 minutes or until the lentils have fallen apart and the carrots are completely cooked.\r\nAfter the soup has cooked and the lentils are tender, blend the soup either in a blender or simply use a hand blender to reach the consistency you desire. Taste for seasoning and add more salt if necessary.\r\nServe with crushed-up crackers, torn up bread, or something else to add some extra thickness.  You could also use a traditional thickener (like cornstarch or flour), but I prefer to add crackers for some texture and saltiness.  Makes great leftovers, stays good in the fridge for about a week.',
-  //     strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-  //     strTags: 'Soup',
-  //     strYoutube: 'https://www.youtube.com/watch?v=VVnZd8A84z4',
-  //     strIngredient1: 'Lentils',
-  //     strIngredient2: 'Onion',
-  //     strIngredient3: 'Carrots',
-  //     strIngredient4: 'Tomato Puree',
-  //     strIngredient5: 'Cumin',
-  //     strIngredient6: 'Paprika',
-  //     strIngredient7: 'Mint',
-  //     strIngredient8: 'Thyme',
-  //     strIngredient9: 'Black Pepper',
-  //     strIngredient10: 'Red Pepper Flakes',
-  //     strIngredient11: 'Vegetable Stock',
-  //     strIngredient12: 'Water',
-  //     strIngredient13: 'Sea Salt',
-  //     strIngredient14: '',
-  //     strIngredient15: '',
-  //     strIngredient16: '',
-  //     strIngredient17: '',
-  //     strIngredient18: '',
-  //     strIngredient19: '',
-  //     strIngredient20: '',
-  //     strMeasure1: '1 cup ',
-  //     strMeasure2: '1 large',
-  //     strMeasure3: '1 large',
-  //     strMeasure4: '1 tbs',
-  //     strMeasure5: '2 tsp',
-  //     strMeasure6: '1 tsp ',
-  //     strMeasure7: '1/2 tsp',
-  //     strMeasure8: '1/2 tsp',
-  //     strMeasure9: '1/4 tsp',
-  //     strMeasure10: '1/4 tsp',
-  //     strMeasure11: '4 cups ',
-  //     strMeasure12: '1 cup ',
-  //     strMeasure13: 'Pinch',
-  //     strMeasure14: ' ',
-  //     strMeasure15: ' ',
-  //     strMeasure16: ' ',
-  //     strMeasure17: ' ',
-  //     strMeasure18: ' ',
-  //     strMeasure19: ' ',
-  //     strMeasure20: ' ',
-  //     strSource: 'https://findingtimeforcooking.com/main-dishes/red-lentil-soup-corba/',
-  //     strImageSource: null,
-  //     strCreativeCommonsConfirmed: null,
-  //     dateModified: null,
-  //   },
-  // ];
+  const [ingredientsCheck, setIngredientsCheck] = useState([]);
+  const [arrayId, setArrayId] = useState([]);
 
   const ingredients = dataRecipe.map((el) => Object.entries(el)
     .filter((entry) => entry[0]
@@ -91,30 +33,80 @@ function RecipeInProgress() {
   const arrayInstructions = Object.entries(objInstructions);
 
   function handleChange({ target }) {
-    console.log(id);
-    const drinks = {
-      drinks: {
-        [id]: [target.className],
-      },
-    };
-    const meals = {
-      meals: {
-        [id]: [target.className],
-      },
-    };
+    setIngredientsCheck([...ingredientsCheck, target.className]);
+    setArrayId([...arrayId, target.className]);
+
     if (target.checked) {
       target.parentNode.className = 'done';
       setCounter(counter + 1);
+      const drinks = {
+        [id]: [...ingredientsCheck, target.className],
+      };
+
+      const meals = {
+        [id]: [...ingredientsCheck, target.className],
+
+      };
       localStorage.setItem(
         'inProgressRecipes',
         JSON.stringify((history.location.pathname === `/meals/${id}/in-progress`)
-          ? [...JSON.parse(localStorage.getItem('inProgressRecipes')), meals] : drinks),
+          ? { ...JSON.parse(localStorage.getItem('inProgressRecipes')), meals }
+          : { ...JSON.parse(localStorage.getItem('inProgressRecipes')), drinks }),
       );
     } else {
       target.parentNode.className = 'undone';
       setCounter(counter - 1);
+      const drinks = {
+        [id]: ingredientsCheck.filter((ingredient) => ingredient !== target.className),
+      };
+
+      const meals = {
+        [id]: ingredientsCheck.filter((ingredient) => ingredient !== target.className),
+
+      };
+      setIngredientsCheck(ingredientsCheck
+        .filter((ingredient) => ingredient !== target.className));
+      localStorage.setItem(
+        'inProgressRecipes',
+        JSON.stringify((history.location.pathname === `/meals/${id}/in-progress`)
+          ? { ...JSON.parse(localStorage.getItem('inProgressRecipes')), meals }
+          : { ...JSON.parse(localStorage.getItem('inProgressRecipes')), drinks }),
+      );
     }
   }
+
+  // const handleCheck = () => {
+  //   console.log('estou aqui');
+  //   if (JSON.parse(localStorage.getItem('inProgressRecipes')) !== null) {
+  //     console.log('estou aqui');
+  //     if (history.location.pathname === `/meals/${id}/in-progress`) {
+  //       const teste = JSON.parse(localStorage.getItem('inProgressRecipes'))
+  //         .meals[id].includes(index);
+  //       console.log(teste);
+  //     }
+  //     const teste1 = JSON.parse(localStorage.getItem('inProgressRecipes'))
+  //       .drinks[id].includes(index);
+  //     console.log(teste1);
+  //   }
+  //   console.log('não entrou');
+  // };
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem('inProgressRecipes')) !== null) {
+      if (history.location.pathname === `/meals/${id}/in-progress`) {
+        setIngredientsCheck(JSON.parse(localStorage.getItem('inProgressRecipes'))
+          .meals[id]);
+        setArrayId(JSON.parse(localStorage.getItem('inProgressRecipes'))
+          .meals[id]);
+        console.log(arrayId);
+      } else {
+        setIngredientsCheck(JSON.parse(localStorage.getItem('inProgressRecipes'))
+          .drinks[id]);
+        setArrayId(JSON.parse(localStorage.getItem('inProgressRecipes'))
+          .drinks[id]);
+      }
+    }
+  }, []);
 
   useEffect(() => {
     if (counter === arrayInstructions.length) {
@@ -126,6 +118,7 @@ function RecipeInProgress() {
 
   return (
     <>
+      {console.log(arrayId)}
       <h1>Em progresso</h1>
       {
         (dataRecipe.length === 0)
@@ -160,6 +153,7 @@ function RecipeInProgress() {
               htmlFor={ ingredient[0] }
             >
               <input
+                checked={ arrayId.includes(index.toString()) }
                 className={ index }
                 type="checkbox"
                 name={ ingredient[0] }
