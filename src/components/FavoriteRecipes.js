5270e70eb3e2fe4b98197524bf4bd22af878332a
Jose Dias <josediasbn1@gmail.com>
2022-12-08 18:30:16 -0300

    reqs 50-56

--- src/components/FavoriteRecipes.js
+++ src/components/FavoriteRecipes.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import copy from 'clipboard-copy';
 import Header from './Header';
+import blackHeart from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
 
 function FavoriteRecipes() {
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
   const [filteredRecipes, setFilteredRecipes] = useState(favoriteRecipes);
+  const [btnCopy, setBtnCopy] = useState(false);
+  const MIL = 1000;
+
+  function delay(milliseconds) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, milliseconds);
+    });
+  }
+
+  async function copiedLinkMsg() {
+    setBtnCopy(true);
+    await delay(MIL);
+    setBtnCopy(false);
+  }
 
   const mealsFilter = () => {
     const aux = favoriteRecipes.filter((el) => el.type === 'meal');
@@ -20,17 +37,44 @@ function FavoriteRecipes() {
     setFilteredRecipes(favoriteRecipes);
   };
 
+  const removeLocal = ({ target }) => {
+    const recipeId = target.parentNode.parentNode.id;
+    const removedRecipe = favoriteRecipes.filter((el) => el.id !== recipeId);
+    const removedRecipeFix = filteredRecipes.filter((el) => el.id !== recipeId);
+    setFilteredRecipes(removedRecipeFix);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(removedRecipe));
+  };
+
   return (
     <>
       <Header />
-      <button type="button" onClick={ mealsFilter }>Meals</button>
-      <button type="button" onClick={ drinksFilter }>Drinks</button>
-      <button type="button" onClick={ allFilter }>All</button>
+      <button
+        type="button"
+        onClick={ mealsFilter }
+        data-testid="filter-by-meal-btn"
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+        onClick={ drinksFilter }
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+      <button
+        type="button"
+        onClick={ allFilter }
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      {(btnCopy === true) ? <p>Link copied!</p> : null}
       {
         filteredRecipes.map((el, index) => {
           if (el.type === 'meal') {
             return (
-              <>
+              <div id={ el.id }>
                 <Link to={ `meals/${el.id}` }>
                   <img
                     src={ el.image }
@@ -52,11 +96,30 @@ function FavoriteRecipes() {
                 <p data-testid={ `${index}-horizontal-top-text` }>
                   { `${el.nationality} - ${el.category}` }
                 </p>
-              </>
+                <button
+                  type="button"
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  onClick={ () => {
+                    copiedLinkMsg();
+                    copy(`http://localhost:3000/meals/${el.id}`);
+                  } }
+                  src={ shareIcon }
+                >
+                  <img src={ shareIcon } alt="compartilhar" />
+                </button>
+                <button
+                  type="button"
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  src={ blackHeart }
+                  onClick={ removeLocal }
+                >
+                  <img src={ blackHeart } alt="coração" />
+                </button>
+              </div>
             );
           }
           return (
-            <>
+            <div id={ el.id } key={ index }>
               <Link to={ `drinks/${el.id}` }>
                 <img
                   src={ el.image }
@@ -77,7 +140,26 @@ function FavoriteRecipes() {
               <p key={ index } data-testid={ `${index}-horizontal-top-text` }>
                 { el.alcoholicOrNot }
               </p>
-            </>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                onClick={ () => {
+                  copiedLinkMsg();
+                  copy(`http://localhost:3000/drinks/${el.id}`);
+                } }
+              >
+                <img src={ shareIcon } alt="compartilhar" />
+              </button>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+                onClick={ removeLocal }
+                src={ blackHeart }
+              >
+                <img src={ blackHeart } alt="coração" />
+              </button>
+            </div>
           );
         })
       }
