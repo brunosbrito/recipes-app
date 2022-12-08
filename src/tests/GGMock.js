9a3bd4541f9ae6554d0beca2d8c447fc1ecef82d
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-12-08 18:27:52 -0300

    [Feat] :rocket: Testes do requisito 29 passando

---
+++ src/tests/GGMock.js
@@ -0,0 +1,32 @@
+const GGMock = {
+  drinks:
+  [
+    {
+      idDrink: '15997',
+      strDrink: 'GG',
+      strDrinkAlternate: null,
+      strTags: null,
+      strVideo: null,
+      strCategory: 'Ordinary Drink',
+      strIBA: null,
+      strAlcoholic: 'Optional alcohol',
+      strGlass: 'Collins Glass',
+      strInstructions: 'Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.',
+      strInstructionsES: null,
+      strInstructionsDE: 'Den Galliano-Likör über Eis gießen. Füllen Sie den Rest des Glases mit Ginger Ale und das ist alles, was dazu gehört. Du hast jetzt ein eigenes GG.',
+      strInstructionsFR: null,
+      strInstructionsIT: 'Versare il liquore Galliano su ghiaccio.\r\nRiempi il resto del bicchiere con ginger ale e questo è tutto.\r\nOra hai il tuo GG personale.',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+      strIngredient1: 'Galliano',
+      strIngredient2: 'Ginger ale',
+      strIngredient3: 'Ice',
+      strMeasure1: '2 1/2 shots ',
+      strImageSource: null,
+      strImageAttribution: null,
+      strCreativeCommonsConfirmed: 'No',
+      dateModified: '2016-07-18 22:06:00',
+    },
+  ],
+};
+
+export default GGMock;
---
+++ src/tests/RecipeDetailsMeals.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { screen, act, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+import corbaMock from './CorbaMock';
+
+const emailInputStr = 'email-input';
+const passInputStr = 'password-input';
+const loginSbmt = 'login-submit-btn';
+const emailEx = 'jose@gmail.com';
+
+describe('Testando o componente RecipeDetails', () => {
+  it('1 - Testa se carrega os elementos da receita "Corba" na página', async () => {
+    const { history } = renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(emailInputStr);
+    const passInput = screen.getByTestId(passInputStr);
+    const buttonInput = screen.getByTestId(loginSbmt);
+    userEvent.type(emailInput, emailEx);
+    userEvent.type(passInput, '1234567');
+    userEvent.click(buttonInput);
+
+    act(() => {
+      history.push('/meals/52977');
+    });
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(corbaMock),
+    });
+
+    await waitFor(() => {
+      const photo = screen.getByTestId('recipe-photo');
+      expect(photo).toBeInTheDocument();
+    });
+
+    const title = screen.getByTestId('recipe-title');
+    expect(title).toHaveTextContent('Corba');
+
+    const category = screen.getByTestId('recipe-category');
+    expect(category).toHaveTextContent('Side');
+  });
+});
